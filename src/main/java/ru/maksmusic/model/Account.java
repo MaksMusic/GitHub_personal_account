51ee87101008e4915bf4461bb2c8f974d4e250d7
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-04-07 23:14:21 +0400

    created classes

--- src/main/java/ru/maksmusic/model/Account.java
+++ src/main/java/ru/maksmusic/model/Account.java
@@ -1,7 +1,62 @@
 package ru.maksmusic.model;
 
 
+import java.util.Objects;
 
 public abstract class Account {
-    String name;
+   String id;
+   String login;
+   String password;
+
+    public Account(String id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id) && Objects.equals(login, account.login) && Objects.equals(password, account.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password);
+    }
 }
--- src/main/java/ru/maksmusic/model/UserAccount.java
+++ src/main/java/ru/maksmusic/model/UserAccount.java
@@ -3,5 +3,9 @@ package ru.maksmusic.model;
 /**
  * UserAccount: класс для представления пользователя, который содержит
  */
-public class UserAccount {
+public class UserAccount  extends Account{
+    public UserAccount(String id, String login, String password) {
+        super(id, login, password);
+    }
+
 }
