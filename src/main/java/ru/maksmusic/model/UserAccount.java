d26f084b2876adcf8da0f4492b32d0727394e993
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-04-08 00:34:26 +0400

    edit gitignore and add field classes UserAccount

--- src/main/java/ru/maksmusic/model/UserAccount.java
+++ src/main/java/ru/maksmusic/model/UserAccount.java
@@ -4,8 +4,12 @@ package ru.maksmusic.model;
  * UserAccount: класс для представления пользователя, который содержит
  */
 public class UserAccount  extends Account{
+    private double balance;
     public UserAccount(String id, String login, String password) {
         super(id, login, password);
     }
 
+    public double getBalance() {
+        return balance;
+    }
 }
