82f0e520e1dc6e94e8d1aeba85fb3eb5e861a817
MaksMusic <Maksmusic05@yandex.ru>
2024-04-10 19:21:12 +0300

    create branch and add Scanner in class Menu

--- src/main/java/ru/maksmusic/view/Menu.java
+++ src/main/java/ru/maksmusic/view/Menu.java
@@ -2,11 +2,14 @@ package ru.maksmusic.view;
 
 import ru.maksmusic.service.UserService;
 
+import java.util.Scanner;
+
 /**
  * Menu: класс для управления пользовательским интерфейсом и взаимодействия с
  */
 public class Menu {
     UserService userService = new UserService();
+    private Scanner scanner = new Scanner(System.in);
     public void start(){
         System.out.println("1 registration user");
         System.out.println("2 registration admin");
