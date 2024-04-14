e1bea774fe352a6aae66d5d6a9bc97558073b7e0
DmitriyRND <dim3333333333@yandex.ru>
2024-04-14 19:59:00 +0300

    добавлен метод сортировки Аккаунтов по логину в класс UserService

--- src/main/java/ru/maksmusic/service/UserService.java
+++ src/main/java/ru/maksmusic/service/UserService.java
@@ -1,10 +1,12 @@
 package ru.maksmusic.service;
 
+import ru.maksmusic.dataBase.AccountDatabase;
+import ru.maksmusic.model.Account;
 import ru.maksmusic.model.UserAccount;
 import ru.maksmusic.repository.UserRepository;
 import ru.maksmusic.repository.UserRepositoryImpl;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class UserService {
 
@@ -30,6 +32,13 @@ public class UserService {
     }
     public void fildUserBy(long id){
         userRepository.fildUserBy(id);
+    }
+
+    public void getAllUsersSortName(){
+        AccountDatabase accountDatabase = new AccountDatabase();
+        List<UserAccount> userAccounts = accountDatabase.getUserAccounts();
+        Collections.sort(userAccounts, Comparator.comparing(Account::getLogin));
+
     }
 
 }
